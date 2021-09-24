cceeaf40e1802ff68098239f9c4f363d2f751c86
Sanketh149 <89764074+Sanketh149@users.noreply.github.com>
2021-09-24 13:05:14 +0530

    day 1 assignment submitted

---
+++ day1/day 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a = 1;
+int b = 1;
+while(b < n) //here n will be given as an input
+    //To decide and analyze time complexity we assume input and find it which will be O(root(n))
+{
+  a += 1;
+  b += a;
+  cout<<"Hi"<<endl;
+}
+}
